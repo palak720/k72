ec37242216a9b1d9169380829f63c7c2261a70da
Palakkasaudhan <Palakkasaudhan123@gamil.com>
2025-09-10 22:19:46 +0530

    homehero section have made

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
 
 import React from 'react'
+import Video from '../components/home/Video'
+import HomeHeroText from '../components/home/HomeHeroText'
+import HomeBottomText from '../components/home/HomeBottomText'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+    <div className='h-screen w-screen fixed'>
+        <Video />
+     </div>
+     <div className='h-screen w-screen relative flex flex-col justify-between'>
+      <HomeHeroText />
+      <HomeBottomText />
+     </div>
+    </div>
   )
 }
 
